b9ef2b30e60716b98392852c37b4aa5d6cc664c8
Daesy Vences <daesydesign@gmail.com>
2021-06-09 22:43:36 -0500

    first commit of Context Clues

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var friends = ["Daze", "Rico", "JC", "Javi", "Yari"];
+    var locations = ["rice cooker", "gym mat", "nachos", "backyard", "fruit display", "safe", "flowerpot", "garden", "study", "gazebo"];
+    var weapons = ["wand", "bat", "glock", "pen", "scissors", "toothbrush", "rock", "nunchucks", "bottle", "kettlebell", "card", "iPhone", "fart gun", "AK-47", "chopsticks", "butterknife", "uzi", "skates", "sombrero", "machete"];
+
+    for (var i = 1; i <= 100; i++) {
+        var $h3 = $("<h3>Accusation " + i + "</h3>");
+        $h3.appendTo($("main"));
+        $h3.click(getAlert(i));
+    }
+
+    function getAlert(i) {
+        var friend = friends[i % 5];
+        var location = locations[i % 10];
+        var weapon = weapons[i % 20];
+        function accuAlert() {
+            alert("Accusation " + i + ": I accuse " + friend + ", with the " + weapon + " in the " + location + "!");
+            var $h2 = $("<h2>Accusation " + i + ": I accuse " + friend + ", with the " + weapon + " in the " + location + "!</h2>");
+            $h2.appendTo("main");
+        }
+        return accuAlert;
+    }
+});
